5331b9e3fdc919bd0c1295e8d9576d5fa6317245
iqbal89co <iqbalfakhriza089@gmail.com>
2025-01-21 08:30:47 +0700

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Controllers;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,10 +11,12 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(opt =>
 {
     opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
